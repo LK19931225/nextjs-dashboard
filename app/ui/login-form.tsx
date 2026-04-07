0941960afa05a259863730fd7f7617a37c0eb153
liukai <14293040202@qq.com>
2026-04-07 09:10:01 +0800

    feat: 添加认证

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,8 @@
+'use client';
 import { lusitana } from '@/app/ui/fonts';
+import { authenticate } from '@/app/lib/actions';
+import { useActionState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -8,8 +12,14 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 
 export default function LoginForm() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined,
+  );
   return (
-    <form className="space-y-3">
+    <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -55,11 +65,22 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
+        {/* 在 HTML 规范里，放在 <form> 里的 <button> 若没有写 type，默认值是 submit。 */}
+        <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
         </div>
       </div>
     </form>
